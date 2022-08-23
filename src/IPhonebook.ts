545324744403bec7085fb59d48b5b933c989b8b9
oferovadia <98873133+oferovadia@users.noreply.github.com>
2022-08-23 16:30:14 +0300

    Update IPhonebook.ts

--- src/IPhonebook.ts
+++ src/IPhonebook.ts
@@ -9,13 +9,3 @@ export interface IPhonebook{
     remove(id: number): Contact | undefined
     [Symbol.iterator](): any
 }
-
-
-// export interface IPhonebook extends Iterable<Contact|undefined> {
-//     size(): number
-//     add(contact: Contact): number
-//     addPhone(id: number, phone: string): void
-//     get(id: number): Contact | undefined
-//     get(name: string): Contact[] | undefined
-//     remove(id: number): Contact | undefined
-// }
